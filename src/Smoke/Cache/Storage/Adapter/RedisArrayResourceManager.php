42bdbe8a1e0217abebc9af0440172787e87ad5ae
Alexander Over <alexander.over@check24.de>
2019-07-24 20:56:34 +0200

    use first server in info response from array

--- src/Smoke/Cache/Storage/Adapter/RedisArrayResourceManager.php
+++ src/Smoke/Cache/Storage/Adapter/RedisArrayResourceManager.php
@@ -66,9 +66,11 @@ class RedisArrayResourceManager
             }
 
             if (! $resource['version']) {
+                $info = $resource['resource']->info();
+
                 // NOTE: We take the first server info as basis for capabilities, as there seems to be no better
                 // option for RedisArray
-                $info = $resource['resource']->info();
+                $info = current($info);
                 $resource['version'] = $info['redis_version'];
             }
 
